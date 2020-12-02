944200ae84bd79dc7df2d615dff38a11109b3fd6
mikeystewartap <mikey@appointedd.com>
2020-12-02 21:15:37 +0000

    feat: attempt at 2nd Wedensday, my part 2 fails though

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-console */
 const FirstTuesday = require('./solutions/1st-tuesday');
+const SecondWednesday = require('./solutions/2nd-wednesday');
 
 const main = async () => {
   const firstTuesdayResult = await FirstTuesday();
   console.log('1st Tuesday: ', firstTuesdayResult);
+
+  const secondTuesdayResult = await SecondWednesday();
+  console.log('2nd Wednesday: ', secondTuesdayResult);
 };
 
 main();
---
+++ solutions/2nd-wednesday.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+module.exports = async () => {
+  const inputString = await fs.readFileSync('./input/2nd-wednesday-part1.txt', 'utf8');
+
+  const passwords = inputString.split('\n').map((rawPasswordData) => {
+    const passwordInfo = rawPasswordData.split(' ');
+    const characterCountMinMax = passwordInfo[0].split('-');
+    return {
+      characterCountMin: parseInt(characterCountMinMax[0], 10),
+      characterCountMax: parseInt(characterCountMinMax[1], 10),
+      character: passwordInfo[1].replace(':', '').trim(),
+      value: passwordInfo[2].trim(),
+    };
+  });
+
+  // Part 1
+  const validPasswordsPartOne = passwords.filter((password) => {
+    const charCount = password.value.split(password.character).length - 1;
+    return charCount >= password.characterCountMin && charCount <= password.characterCountMax;
+  });
+
+  // Part 2
+  const validPasswordsPartTwo = passwords.filter((password) => {
+    const characterOneValue = password.value[password.characterCountMin - 1];
+    const characterTwoValue = password.value[password.characterCountMax - 1];
+    return characterOneValue === password.character || characterTwoValue === password.character;
+  });
+
+  return {
+    partOne: validPasswordsPartOne.length,
+    partTwo: validPasswordsPartTwo.length,
+  };
+};
