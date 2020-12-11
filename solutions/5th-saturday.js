5b71747933f9cec14c5d1e3a24270a881fc3097e
mikeystewartap <mikey@appointedd.com>
2020-12-11 21:09:44 +0000

    feat: Solution to 5th Saturday

--- solutions/5th-saturday.js
+++ solutions/5th-saturday.js
@@ -22,15 +22,15 @@ const getValueFromRange = (rangeString, maxCount, lowerChar) => {
       }
     } else {
       const rangeDiff = maxRange - minRange;
-      const halfOfDiff = Math.floor(rangeDiff / 2);
-      minRange = char === lowerChar ? minRange : minRange + +halfOfDiff;
-      maxRange = char === lowerChar ? maxRange - halfOfDiff : maxRange;
+      const halfOfDiffUp = Math.ceil(rangeDiff / 2);
+      const halfOfDiffDown = Math.floor(rangeDiff / 2);
+      minRange = char === lowerChar ? minRange : minRange + +halfOfDiffUp;
+      maxRange = char === lowerChar ? maxRange - halfOfDiffDown - 1 : maxRange;
     }
 
     lastChar = char;
   }
-
-  return lastChar === lowerChar ? minRange : maxRange;
+  return (lastChar === lowerChar ? minRange : maxRange);
 };
 
 const getRow = (partitionString) => {
@@ -41,7 +41,24 @@ const getColumn = (partitionString) => {
   const columnDataString = partitionString.slice(7);
   return getValueFromRange(columnDataString, maxColumnCount, 'L');
 };
-const getSeatId = (row, column) => row * 8 + column;
+const getSeatId = (row, column) => row * 8 + +column;
+
+const getMissingTicketId = (boardingPasses) => {
+  const seatIds = [];
+  boardingPasses.forEach((boardingPass) => {
+    seatIds.push(boardingPass.seatId);
+  });
+
+  for (let i = 0; i < seatIds.length; i += 1) {
+    const seat = seatIds[i];
+    const seatAsIndex = seat - 6;
+
+    if (i !== seatAsIndex) {
+      return seat - 1;
+    }
+  }
+  return 0;
+};
 
 module.exports = async () => {
   const inputString = await fs.readFileSync('./input/5th-saturday.txt', 'utf8');
@@ -56,13 +73,13 @@ module.exports = async () => {
     };
   });
 
-  // Part 1
   const boardingPassesSortedBySeatId = boardingPasses.sort((a, b) => ((a.seatId > b.seatId) ? 1 : -1));
 
-  const partTwo = [];
+  const partOne = boardingPassesSortedBySeatId[boardingPassesSortedBySeatId.length - 1].seatId;
+  const partTwo = getMissingTicketId(boardingPasses);
 
   return {
-    partOne: boardingPassesSortedBySeatId[boardingPassesSortedBySeatId.length - 1].seatId,
-    partTwo: partTwo.length,
+    partOne,
+    partTwo,
   };
 };
